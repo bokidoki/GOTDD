0c229f196348b02a9ed859fe2af78997f863de63
bokidoki <280327097@qq.com>
2021-12-24 09:26:12 +0800

    add test

--- src/inter/api_backend.go
+++ src/inter/api_backend.go
@@ -1,8 +1,14 @@
 package inter
 
+import "math/big"
+
 type EthApiBackend struct {
 }
 
 func (b *EthApiBackend) SyncProgress() int {
 	return 1
+}
+
+func (b *EthApiBackend) FeeHistory() big.Int {
+	return *big.NewInt(1)
 }
